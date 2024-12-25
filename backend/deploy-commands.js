71ecd6b0ec7d48d62c2cc3f8fb87df713ff37a50
Jacin <philj038@gmail.com>
2024-12-24 19:07:08 -0500

    asdf

--- backend/deploy-commands.js
+++ backend/deploy-commands.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const commands = [];
-const commandsPath = path.join(__dirname, "Commands");
+const commandsPath = path.join(__dirname, "commands");
 const commandFiles = fs
   .readdirSync(commandsPath)
   .filter((file) => file.endsWith(".js"));
